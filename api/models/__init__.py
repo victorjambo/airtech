86a0fad01c8e4fd3d2c64fb783d51984cc348aa6
victorjambo <victorjambo@live.com>
2019-07-18 01:27:18 +0300

    [Chore] Tickets model with relationship

--- api/models/__init__.py
+++ api/models/__init__.py
@@ -4,6 +4,7 @@ from api.utilities.push_id import PushID
 
 from api.models.users import User
 from api.models.flights import Flight
+from api.models.tickets import Ticket
 
 def fancy_id_generator(mapper, connection, target):
     """A function to generate unique identifiers on insert."""
@@ -13,7 +14,7 @@ def fancy_id_generator(mapper, connection, target):
 
 # associate the listener function with models, to execute during the
 # "before_insert" event
-tables = [User, Flight]
+tables = [User, Flight, Ticket]
 
 for table in tables:
     event.listen(table, 'before_insert', fancy_id_generator)
---
+++ api/models/tickets.py
@@ -0,0 +1,17 @@
+from .config.database import db
+from .config.auditable_model import AuditableBaseModel
+
+class Ticket(AuditableBaseModel):
+    """Class Ticket model"""
+    __tablename__ = 'tickets'
+
+    seat_number = db.Column(db.String(), unique=True, nullable=False)
+    destination = db.Column(db.String(), nullable=False)
+    travel_date = db.Column(db.String(), nullable=False)
+
+    user_id = db.Column(db.String, db.ForeignKey('users.id'))
+    flight_id = db.Column(db.String, db.ForeignKey('flights.id'))
+
+
+    def __repr__(self):
+        return f'<Ticket {self.seat_number}>'
